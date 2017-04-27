e2972ff2f1483b926d2216a38ba9b50b15b96bac
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2017-04-27 07:11:34 +0300

    Small change

--- app/Routes.php
+++ app/Routes.php
@@ -24,7 +24,7 @@ $router->get('sample/{name?}/{slug?}', 'App\Controllers\Sample@index');
 $router->post('sample', 'App\Controllers\Sample@store');
 
 
-$router->get('test/{id?}/{name?}/{slug?}', array(function ($id, $name = null, $slug = null)
+$router->get('test/{id}/{name?}/{slug?}', array(function ($id, $name = null, $slug = null)
 {
     return array('id' => $id, 'name' => $name, 'slug' => $slug);
 
