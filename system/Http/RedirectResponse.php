0caf2d74709088217609f270dcc5fc7ae13d6efc
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2017-05-06 03:40:32 +0300

    Improve Mini\Http\Response

--- system/Http/RedirectResponse.php
+++ system/Http/RedirectResponse.php
@@ -6,11 +6,26 @@ use Mini\Http\ResponseTrait;
 
 use Symfony\Component\HttpFoundation\Cookie as SymfonyCookie;
 use Symfony\Component\HttpFoundation\RedirectResponse as SymfonyRedirectResponse;
+use Symfony\Component\HttpFoundation\File\UploadedFile as SymfonyUploadedFile;
 
 
 class RedirectResponse extends SymfonyRedirectResponse
 {
     use ResponseTrait;
+
+    /**
+     * The request instance.
+     *
+     * @var \Http\Request
+     */
+    protected $request;
+
+    /**
+     * The session store implementation.
+     *
+     * @var \Nova\Session\Store
+     */
+    protected $session;
 
 
     /**
@@ -26,6 +41,88 @@ class RedirectResponse extends SymfonyRedirectResponse
         }
 
         return $this;
+    }
+
+    /**
+     * Flash an array of input to the session.
+     *
+     * @param  array  $input
+     * @return $this
+     */
+    public function withInput(array $input = null)
+    {
+        $input = $input ?: $this->request->input();
+
+        $this->session->flashInput(array_filter($input, function ($value)
+        {
+            return ! $value instanceof SymfonyUploadedFile;
+        }));
+
+        return $this;
+    }
+
+    /**
+     * Flash an array of input to the session.
+     *
+     * @param  mixed  string
+     * @return $this
+     */
+    public function onlyInput()
+    {
+        return $this->withInput($this->request->only(func_get_args()));
+    }
+
+    /**
+     * Flash an array of input to the session.
+     *
+     * @param  mixed  string
+     * @return \Nova\Http\RedirectResponse
+     */
+    public function exceptInput()
+    {
+        return $this->withInput($this->request->except(func_get_args()));
+    }
+    
+    /**
+     * Get the request instance.
+     *
+     * @return  \Nova\Http\Request
+     */
+    public function getRequest()
+    {
+        return $this->request;
+    }
+
+    /**
+     * Set the request instance.
+     *
+     * @param  \Nova\Http\Request  $request
+     * @return void
+     */
+    public function setRequest(Request $request)
+    {
+        $this->request = $request;
+    }
+
+    /**
+     * Get the session store implementation.
+     *
+     * @return \Session\Store
+     */
+    public function getSession()
+    {
+        return $this->session;
+    }
+
+    /**
+     * Set the session store implementation.
+     *
+     * @param  \Session\Store  $session
+     * @return void
+     */
+    public function setSession(SessionStore $session)
+    {
+        $this->session = $session;
     }
 
 }
