ce19b1cb232268f4a839662b09f9b5bca7b3060c
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2017-05-06 21:23:31 +0300

    Initial implementation of the Validation Service

---
+++ system/Support/Facades/Validator.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Mini\Support\Facades;
+
+use Mini\Support\Facades\Facade;
+
+
+/**
+ * @see \Mini\Validation\Factory
+ */
+class Validator extends Facade
+{
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor() { return 'validator'; }
+
+}
