2f189fde446c0ea606b12d6e77e86bec6e4f544c
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2017-05-08 16:04:37 +0300

    Improve Mini\Database\ORM\Relation

--- system/Database/ORM/Relation.php
+++ system/Database/ORM/Relation.php
@@ -14,15 +14,22 @@ class Relation
 
 	protected $getter;
 
+	/**
+	 * All of the registered relation macros.
+	 *
+	 * @var array
+	 */
+	protected $macros = array();
 
-    /**
-     * Create a new relation instance.
-     *
-     * @param  \Mini\Database\ORM\Builder  $query
-     * @param  \Mini\Database\ORM\Model  $parent
-     * @param string $getter
-     * @return void
-     */
+
+	/**
+	 * Create a new relation instance.
+	 *
+	 * @param  \Mini\Database\ORM\Builder  $query
+	 * @param  \Mini\Database\ORM\Model  $parent
+	 * @param string $getter
+	 * @return void
+	 */
 	public function __construct(Builder $query, Model $parent, $getter = 'first')
 	{
 		$this->query = $query;
@@ -32,14 +39,26 @@ class Relation
 		$this->getter = $getter;
 	}
 
-    /**
-     * Get the result(s) of the relationship.
-     *
-     * @return mixed
-     */
+	/**
+	 * Get the result(s) of the relationship.
+	 *
+	 * @return mixed
+	 */
 	public function getResults()
 	{
 		return call_user_func(array($this->query, $this->getter));
+	}
+
+	/**
+	 * Extend the relation with a given callback.
+	 *
+	 * @param  string	$name
+	 * @param  \Closure  $callback
+	 * @return void
+	 */
+	public function macro($name, Closure $callback)
+	{
+		$this->macros[$name] = $callback;
 	}
 
 	/**
@@ -51,6 +70,12 @@ class Relation
 	 */
 	public function __call($method, $parameters)
 	{
+		if (isset($this->macros[$method])) {
+			array_unshift($parameters, $this);
+
+			return call_user_func_array($this->macros[$method], $parameters);
+		}
+
 		return call_user_func_array(array($this->query, $method), $parameters);
 	}
 }
