297b59a154e2a8fc9167084cd929ece9b1beed5a
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2017-05-12 06:19:09 +0300

    Improve the app/Views/Partials/Messages.php

--- app/Views/Partials/Messages.php
+++ app/Views/Partials/Messages.php
@@ -1,16 +1,14 @@
 <?php foreach(array('success', 'warning', 'danger') as $type) { ?>
-
-<?php if (Session::has($type)) { ?>
+	<?php if (Session::has($type)) { ?>
 
 <div class="row">
-	<div class="alert alert-success alert-dismissable">
+	<div class="alert alert-<?= $type; ?> alert-dismissable">
 		<button type="button" class="close" data-dismiss="alert" aria-hidden="true"><span aria-hidden="true">&times;</span></button>
 		<?= Session::get($type); ?>
 	</div>
 </div>
 
-<?php } ?>
-
+	<?php } ?>
 <?php } ?>
 
 <?php if ($errors->any()) { ?>
