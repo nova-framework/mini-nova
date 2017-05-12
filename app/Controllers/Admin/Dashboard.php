a550d57651ea6a0665fa3ec7f0fa10dad0abaab4
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2017-05-12 04:55:10 +0300

    Adding files

---
+++ app/Controllers/Admin/Dashboard.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Dasboard - Implements a simple Administration Dashboard.
+ *
+ * @author Virgil-Adrian Teaca - virgil@giulianaeassociati.com
+ */
+
+namespace App\Controllers\Admin;
+
+use Nova\Support\Facades\Config;
+use Nova\Support\Facades\View;
+
+use App\Core\BackendController;
+
+
+class Dashboard extends BackendController
+{
+
+	public function index()
+	{
+		$debug = '';
+
+		return $this->getView()
+			->shares('title', __('Dashboard'))
+			->with('debug', $debug);
+	}
+
+}
