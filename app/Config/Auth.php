0ccb1c3db8285ec9d0462dffb4d61ba3fea01fdb
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2017-05-13 11:29:54 +0300

    Add app/Config/Auth.php

---
+++ app/Config/Auth.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Auth configuration
+ *
+ * @author Virgil-Adrian Teaca - virgil@giulianaeassociati.com
+ * @version 4.0
+ */
+
+
+return array(
+
+	/*
+	|--------------------------------------------------------------------------
+	| Authentication Defaults
+	|--------------------------------------------------------------------------
+	|
+	| The default authentication "guard" used by your application.
+	|
+	*/
+
+	'default' => 'web',
+
+	/*
+	|--------------------------------------------------------------------------
+	| Authentication Guards
+	|--------------------------------------------------------------------------
+	|
+	| There you may define every authentication guard for your application.
+	|
+	| Supported: "session", "token"
+	|
+	*/
+
+	'guards' => array(
+		'web' => array(
+			'driver' => 'session',
+			'model'  => 'App\Models\User',
+		),
+		'api' => array(
+			'driver' => 'token',
+			'model'  => 'App\Models\User',
+		),
+	),
+
+);
