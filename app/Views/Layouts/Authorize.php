8fe68f069643dcb9c00756d9686cfa5b099d1096
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2017-05-13 22:42:42 +0300

    Overall improvements

--- app/Views/Layouts/Authorize.php
+++ app/Views/Layouts/Authorize.php
@@ -29,7 +29,18 @@
 		<!-- Collect the nav links, forms, and other content for toggling -->
 		<div class="collapse navbar-collapse navbar-ex1-collapse">
 			<ul class="nav navbar-nav navbar-right" style="margin-right: 0;">
-				<?= View::fetch('Partials/LanguageChanger'); ?>
+				<li class="dropdown">
+					<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+						<i class='fa fa-language'></i> <?= Language::name() ?>
+					</a>
+					<ul class="dropdown-menu">
+					<?php foreach (Config::get('languages') as $code => $info) { ?>
+						<li <?= ($code == Language::code()) ? 'class="active"' : ''; ?>>
+							<a href='<?= site_url('language/' .$code) ?>' title='<?= $info['info'] ?>'><?= $info['name'] ?></a>
+						</li>
+					<?php } ?>
+					</ul>
+				</li>
 				<!-- Authentication Links -->
 				<li <?= ($baseUri == 'auth/login') ? 'class="active"' : ''; ?>>
 					<a href="<?= site_url('auth/login'); ?>"><?= __('User Login'); ?></a>
--- app/Views/Layouts/Backend.php
+++ app/Views/Layouts/Backend.php
@@ -44,7 +44,18 @@
 				</li>
 			</ul>
 			<ul class="nav navbar-nav navbar-right" style="margin-right: 0;">
-				<?= View::fetch('Partials/LanguageChanger'); ?>
+				<li class="dropdown">
+					<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+						<i class='fa fa-language'></i> <?= Language::name() ?>
+					</a>
+					<ul class="dropdown-menu">
+					<?php foreach (Config::get('languages') as $code => $info) { ?>
+						<li <?= ($code == Language::code()) ? 'class="active"' : ''; ?>>
+							<a href='<?= site_url('language/' .$code) ?>' title='<?= $info['info'] ?>'><?= $info['name'] ?></a>
+						</li>
+					<?php } ?>
+					</ul>
+				</li>
 				<!-- Authentication Links -->
 				<li class="dropdown <?= ($baseUri == 'admin/profile') ? 'active' : ''; ?>">
 					<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
