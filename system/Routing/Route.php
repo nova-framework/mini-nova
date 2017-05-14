4589a7eee0ed4aa608132dcfb2e29ca5749ef57b
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2017-05-14 16:50:53 +0300

    Improve the Routing

--- system/Routing/Route.php
+++ system/Routing/Route.php
@@ -125,16 +125,10 @@ class Route
 	 */
 	protected function runCallable(Request $request)
 	{
-		$parameters = $this->parameters();
-
 		$callable = $this->action['uses'];
 
-		if (empty($parameters)) {
-			return call_user_func($callable);
-		}
-
 		$parameters = $this->resolveMethodDependencies(
-			$parameters, new ReflectionFunction($callable)
+			$this->parameters(), new ReflectionFunction($callable)
 		);
 
 		return call_user_func_array($callable, $parameters);
--- system/Routing/RouteDependencyResolverTrait.php
+++ system/Routing/RouteDependencyResolverTrait.php
@@ -36,7 +36,7 @@ trait RouteDependencyResolverTrait
 	 */
 	protected function resolveClassMethodDependencies(array $parameters, $instance, $method)
 	{
-		if (! method_exists($instance, $method) || empty($parameters)) {
+		if (! method_exists($instance, $method)) {
 			return $parameters;
 		}
 
