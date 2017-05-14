f821abdd1bf5d4b095c9bde19a274ddf34b322a0
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2017-05-14 03:36:06 +0300

    Update the Language files

--- app/Views/Admin/Settings/Index.php
+++ app/Views/Admin/Settings/Index.php
@@ -1,8 +1,8 @@
 <div class="row">
-	<h1><?= __d('system', 'Settings'); ?></h1>
+	<h1><?= __('Settings'); ?></h1>
 	<ol class="breadcrumb">
-		<li><a href='<?= site_url('admin/dashboard'); ?>'><i class="fa fa-dashboard"></i> <?= __d('system', 'Dashboard'); ?></a></li>
-		<li><?= __d('system', 'Settings'); ?></li>
+		<li><a href='<?= site_url('admin/dashboard'); ?>'><i class="fa fa-dashboard"></i> <?= __('Dashboard'); ?></a></li>
+		<li><?= __('Settings'); ?></li>
 	</ol>
 </div>
 
@@ -17,11 +17,11 @@
 
 <div class="panel panel-default">
 	<div class="panel-body" style="padding-top: 0;">
-		<h3><?= __d('system', 'Site Settings'); ?></h3>
+		<h3><?= __('Site Settings'); ?></h3>
 		<hr style="margin-top: 0;">
 
 		<div class="form-group">
-			<label class="col-sm-4 control-label" for="sitename"><?= __d('system', 'Site Name'); ?></label>
+			<label class="col-sm-4 control-label" for="sitename"><?= __('Site Name'); ?></label>
 			<div class="col-sm-8">
 				<input name="siteName" id="siteName" type="text" class="form-control" value="<?= $options['siteName']; ?>">
 			</div>
@@ -29,105 +29,112 @@
 	</div>
 </div>
 
+<div class="col-lg-12" style="padding-right: 0;">
+	<input class="btn btn-success col-sm-2 pull-right" type="submit" id="submit" name="submit" value="<?= __('Apply the changes') ?>" />&nbsp;
+</div>
+
+<div class="clearfix"></div>
+<br>
+
 <div class="panel panel-default">
 	<div class="panel-body" style="padding-top: 0;">
-		<h3><?= __d('system', 'Mailer Settings'); ?></h3>
+		<h3><?= __('Mailer Settings'); ?></h3>
 		<hr style="margin-top: 0;">
 
 		<div class="form-group">
-			<label class="col-sm-4 control-label" for="mailDriver"><?= __d('system', 'Mail Driver'); ?></label>
+			<label class="col-sm-4 control-label" for="mailDriver"><?= __('Mail Driver'); ?></label>
 			<div class="col-sm-8">
 				<div class="col-sm-3" style="padding: 0;">
 					<select name="mailDriver" id="mailDriver" class="form-control">
-						<option value="smtp" <?php if ($options['mailDriver'] == 'smtp') { echo "selected='selected'"; }?>><?= __d('system', 'SMTP'); ?></option>
-						<option value="mail" <?php if ($options['mailDriver'] == 'mail') { echo "selected='selected'"; }?>><?= __d('system', 'Mail'); ?></option>
-						<option value="sendmail" <?php if ($options['mailDriver'] == 'sendmail') { echo "selected='selected'"; }?>><?= __d('system', 'Sendmail'); ?></option>
+						<option value="smtp" <?php if ($options['mailDriver'] == 'smtp') { echo "selected='selected'"; }?>><?= __('SMTP'); ?></option>
+						<option value="mail" <?php if ($options['mailDriver'] == 'mail') { echo "selected='selected'"; }?>><?= __('Mail'); ?></option>
+						<option value="sendmail" <?php if ($options['mailDriver'] == 'sendmail') { echo "selected='selected'"; }?>><?= __('Sendmail'); ?></option>
 					</select>
 				</div>
 				<div class='clearfix'></div>
-				<small><?= __d('system', 'Whether or not is used a external SMTP Server to send the E-mails.'); ?></small>
+				<small><?= __('Whether or not is used a external SMTP Server to send the E-mails.'); ?></small>
 			</div>
 		</div>
 		<div class="form-group">
-			<label class="col-sm-4 control-label" for="mailFromAddress"><?= __d('system', 'E-mail From Address'); ?></label>
+			<label class="col-sm-4 control-label" for="mailFromAddress"><?= __('E-mail From Address'); ?></label>
 			<div class="col-sm-8">
 				<div class="col-sm-6" style="padding: 0;">
 					<input name="mailFromAddress" id="mailFromAddress" type="text" class="form-control" value="<?= $options['mailFromAddress']; ?>">
 				</div>
 				<div class='clearfix'></div>
-				<small><?= __d('system', 'The outgoing E-mail address.'); ?></small>
+				<small><?= __('The outgoing E-mail address.'); ?></small>
 			</div>
 		</div>
 		<div class="form-group">
-			<label class="col-sm-4 control-label" for="mailFromName"><?= __d('system', 'E-mail From Name'); ?></label>
+			<label class="col-sm-4 control-label" for="mailFromName"><?= __('E-mail From Name'); ?></label>
 			<div class="col-sm-8">
 				<div class="col-sm-6" style="padding: 0;">
 					<input name="mailFromName" id="mailFromName" type="text" class="form-control" value="<?= $options['mailFromName']; ?>">
 				</div>
 				<div class='clearfix'></div>
-				<small><?= __d('system', 'The From Field of any outgoing e-mails.'); ?></small>
+				<small><?= __('The From Field of any outgoing e-mails.'); ?></small>
 			</div>
 		</div>
 
 		<div class="form-group">
-			<label class="col-sm-4 control-label" for="mailHost"><?= __d('system', 'Server Name'); ?></label>
+			<label class="col-sm-4 control-label" for="mailHost"><?= __('Server Name'); ?></label>
 			<div class="col-sm-8">
 				<div class="col-sm-6" style="padding: 0;">
 					<input name="mailHost" id="mailHost" type="text" class="form-control" value="<?= $options['mailHost']; ?>">
 				</div>
 				<div class='clearfix'></div>
-				<small><?= __d('system', 'The name of the external SMTP Server.'); ?></small>
+				<small><?= __('The name of the external SMTP Server.'); ?></small>
 			</div>
 		</div>
 		<div class="form-group">
-			<label class="col-sm-4 control-label" for="mailPort"><?= __d('system', 'Server Port'); ?></label>
+			<label class="col-sm-4 control-label" for="mailPort"><?= __('Server Port'); ?></label>
 			<div class="col-sm-8">
 				<div class="col-sm-2" style="padding: 0;">
 					<input name="mailPort" id="mailPort" type="text" class="form-control" value="<?= $options['mailPort']; ?>">
 				</div>
 				<div class='clearfix'></div>
-				<small><?= __d('system', 'The Port used for connecting to the external SMTP Server.'); ?></small>
+				<small><?= __('The Port used for connecting to the external SMTP Server.'); ?></small>
 			</div>
 		</div>
 		<div class="form-group">
-			<label class="col-sm-4 control-label" for="mailEncryption"><?= __d('system', 'Use the Cryptography'); ?></label>
+			<label class="col-sm-4 control-label" for="mailEncryption"><?= __('Use the Cryptography'); ?></label>
 			<div class="col-sm-8">
 				<div class="col-sm-2" style="padding: 0;">
 					<select name="mailEncryption" id="mailEncryption" class="form-control">
 						<option value="ssl" <?php if ($options['mailEncryption'] == 'ssl') { echo "selected='selected'"; }?>>SSL</option>
 						<option value="tls" <?php if ($options['mailEncryption'] == 'tls') { echo "selected='selected'"; }?>>TLS</option>
-						<option value="" <?php if ($options['mailEncryption'] == '') { echo "selected='selected'"; }?>><?= __d('system', 'NONE'); ?></option>
+						<option value="" <?php if ($options['mailEncryption'] == '') { echo "selected='selected'"; }?>><?= __('NONE'); ?></option>
 					</select>
 				</div>
 				<div class='clearfix'></div>
-				<small><?= __d('system', 'Whether or not is used the Cryptography for connecting to the external SMTP Server.'); ?></small>
+				<small><?= __('Whether or not is used the Cryptography for connecting to the external SMTP Server.'); ?></small>
 			</div>
 		</div>
 		<div class="form-group">
-			<label class="col-sm-4 control-label" for="mailUsername"><?= __d('system', 'Server Username'); ?></label>
+			<label class="col-sm-4 control-label" for="mailUsername"><?= __('Server Username'); ?></label>
 			<div class="col-sm-8">
 				<div class="col-sm-6" style="padding: 0;">
 					<input name="mailUsername" id="mailUsername" type="text" class="form-control" value="<?= $options['mailUsername']; ?>">
 				</div>
 				<div class='clearfix'></div>
-				<small><?= __d('system', 'The Username used to connect to the external SMTP Server.'); ?></small>
+				<small><?= __('The Username used to connect to the external SMTP Server.'); ?></small>
 			</div>
 		</div>
 		<div class="form-group">
-			<label class="col-sm-4 control-label" for="mailPassword"><?= __d('system', 'Server Password'); ?></label>
+			<label class="col-sm-4 control-label" for="mailPassword"><?= __('Server Password'); ?></label>
 			<div class="col-sm-8">
 				<div class="col-sm-6" style="padding: 0;">
 					<input name="mailPassword" id="mailPassword" type="password" class="form-control" value="<?= $options['mailPassword']; ?>">
 				</div>
 				<div class='clearfix'></div>
-				<small><?= __d('system', 'The Password used to connect to the external SMTP Server.'); ?></small>
+				<small><?= __('The Password used to connect to the external SMTP Server.'); ?></small>
 			</div>
 		</div>
 	</div>
 </div>
 
 <div class="col-lg-12" style="padding-right: 0;">
-	<input class="btn btn-success col-sm-2 pull-right" type="submit" id="submit" name="submit" value="<?= __d('system', 'Apply the changes') ?>" />&nbsp;
+	<input class="btn btn-success col-sm-2 pull-right" type="submit" id="submit" name="submit" value="<?= __('Apply the changes') ?>" />&nbsp;
 </div>
 
 <input type="hidden" name="_token" value="<?= csrf_token(); ?>" />
@@ -137,7 +144,7 @@
 <?php } else { ?>
 
 <div class="callout callout-info">
-	<?= __d('system', 'The Settings are not available while the Config Store is on Files Mode.'); ?>
+	<?= __('The Settings are not available while the Config Store is on Files Mode.'); ?>
 </div>
 
 <?php } ?>
