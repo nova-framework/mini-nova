7e003a611938eb19edd4232f6cdffb7308cbf0be
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2017-05-15 14:06:01 +0300

    Complete the Console support

---
+++ app/Console/Kernel.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Console;
+
+use Mini\Foundation\Console\Kernel as ConsoleKernel;
+
+
+class Kernel extends ConsoleKernel
+{
+	/**
+	 * The Forge commands provided by the application.
+	 *
+	 * @var array
+	 */
+	protected $commands = array();
+
+}
