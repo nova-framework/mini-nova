734e44d77a1960d5b4868476ab56e2c0354d0ec8
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2017-05-19 14:57:15 +0300

    Initial implementation of the FileField plugin

---
+++ plugins/FileField/Providers/PluginServiceProvider.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace FileField\Providers;
+
+use Mini\Support\ServiceProvider;
+
+
+class PluginServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the Application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $path = realpath(__DIR__ .'/../');
+
+        // Configure the Package.
+        $this->package('Plugins/FileField', 'file_field', $path);
+
+        //
+    }
+
+    /**
+     * Register the Service Provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
