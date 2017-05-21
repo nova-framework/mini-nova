8eb0f9a08b2c1b886fdf162946090f29051ad806
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2017-05-21 16:59:33 +0300

    Improve the Content plugin

--- plugins/Content/src/Providers/PluginServiceProvider.php
+++ plugins/Content/src/Providers/PluginServiceProvider.php
@@ -2,11 +2,29 @@
 
 namespace Content\Providers;
 
-use Mini\Support\ServiceProvider;
+use Mini\Auth\Contracts\Access\GateInterface as Gate;
+use Mini\Routing\Router;
+use Mini\Plugin\Support\Providers\PluginServiceProvider as ServiceProvider;
 
 
 class PluginServiceProvider extends ServiceProvider
 {
+	/**
+	 * The additional provider class names.
+	 *
+	 * @var array
+	 */
+	protected $providers = array();
+
+	/**
+	 * The policy mappings for the plugin.
+	 *
+	 * @var array
+	 */
+	protected $policies = array(
+		'Content\Models\SomeModel' => 'Content\Policies\ModelPolicy',
+	);
+
 	/**
 	 * This namespace is applied to the controller routes in your routes file.
 	 *
@@ -27,15 +45,22 @@ class PluginServiceProvider extends ServiceProvider
 		// Configure the Package.
 		$this->package('Content', 'content', $path);
 
-		// Load the Plugin Bootstrap.
+		// Bootstrap the Plugin.
 		require $path .DS .'Bootstrap.php';
+
+		// Load the Plugin Policies.
+		$gate = $this->app->make(Gate::class);
+
+		foreach ($this->policies as $key => $value) {
+			$gate->policy($key, $value);
+		}
 
 		// Load the Plugin Routes.
 		$router = $this->app['router'];
 
 		$router->group(array('namespace' => $this->namespace), function ($router) use ($path)
 		{
-			require $path .DS .'Routes.php';
+			require  $path .DS .'Routes.php';
 		});
 	}
 
@@ -46,6 +71,8 @@ class PluginServiceProvider extends ServiceProvider
 	 */
 	public function register()
 	{
+		parent::register();
+
 		//
 	}
 }
