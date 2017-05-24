38e6c5e6001cca533e501579db948af01c317aa5
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2017-05-24 07:35:58 +0300

    Improve the Backend plugin

--- plugins/Backend/src/Views/Admin/Messages/Index.php
+++ plugins/Backend/src/Views/Admin/Messages/Index.php
@@ -10,8 +10,15 @@
 
 <!-- Main content -->
 <div class="row">
+	<h3><?= __d('backend', 'Manage the Messages'); ?></h3>
+	<br>
+	<a class='btn btn-success' href='<?= site_url('admin/messages/create'); ?>'><i class='fa fa-send'></i> <?= __d('backend', 'Send a new Message'); ?></a>
+	<hr>
+</div>
+
+<div class="row">
 	<h3><?= __d('backend', 'Conversations'); ?></h3>
-	<hr style="margin-bottom: 10px;">
+	<br>
 
 <style>
 
@@ -21,8 +28,10 @@
 
 </style>
 
+<?php if (! $messages->isEmpty()) { ?>
+	<div class="list-group">
+
 <?php
-if (! $messages->isEmpty()) {
 	$count = 0;
 
 	$total = $messages->count();
@@ -38,42 +47,41 @@ if (! $messages->isEmpty()) {
 			$unread++;
 		}
 ?>
-	<!-- Statuses -->
-	<div class="media" style="margin-top: 0;">
-		<div class="pull-left">
-			<img class="img-thumbnail media-object img-responsive" style="height: 75px; width: 75px" alt="<?= $message->sender->fullName(); ?>" src="<?= $message->sender->picture(); ?>">
-		</div>
-		<div class="media-body">
-			<div class="col-md-8 no-padding">
-				<h4 class="media-heading"><a href="<?= site_url('admin/messages/' .$message->id); ?>"><?= e($message->subject); ?></a> <?php if ($unread >  0) echo '<small class="label label-warning">' .$unread .'</small>'; ?></h4>
-				<p class="no-margin"><?= __d('backend', 'From <b>{0}</b>, to <b>{1}</b>', $message->sender->fullName(), $message->receiver->fullName()); ?></p>
-				<ul class="list-inline text-muted no-margin">
-					<li><?= __d('backend', '{0, plural, one{# reply} other{# replies}}', $message->replies->count()); ?></li>
-					<li><?= $message->created_at->diffForHumans(); ?></li>
-				</ul>
+		<!-- Statuses -->
+		<div class="list-group-item" style="padding-right: 10px;">
+			<div class="media">
+				<div class="pull-left">
+					<img class="img-thumbnail media-object img-responsive" style="height: 75px; width: 75px" alt="<?= $message->sender->fullName(); ?>" src="<?= $message->sender->picture(); ?>">
+				</div>
+				<div class="media-body">
+					<div class="col-md-8">
+						<h4 class="media-heading"><a href="<?= site_url('admin/messages/' .$message->id); ?>"><?= e($message->subject); ?></a> <?php if ($unread >  0) echo '<small class="label label-warning">' .$unread .'</small>'; ?></h4>
+						<p class="no-margin"><?= __d('backend', 'From <b>{0}</b>, to <b>{1}</b>', $message->sender->fullName(), $message->receiver->fullName()); ?></p>
+						<ul class="list-inline text-muted no-margin">
+							<li><?= __d('backend', '{0, plural, one{# reply} other{# replies}}', $message->replies->count()); ?></li>
+							<li><?= $message->created_at->diffForHumans(); ?></li>
+						</ul>
+					</div>
+					<div class="col-md-4 no-padding">
+						<a class="btn btn-warning pull-right" title="<?= __d('backend', 'View this message and its replies'); ?>" href="<?= site_url('admin/messages/' .$message->id); ?>"><i class='fa fa-search'></i></a>
+						<div class="clearfix"></div>
+					</div>
+				</div>
 			</div>
-			<div class="col-md-4 no-padding">
-				<a class="btn btn-warning pull-right" title="<?= __d('backend', 'View this message and its replies'); ?>" href="<?= site_url('admin/messages/' .$message->id); ?>"><i class='fa fa-search'></i></a>
-				<div class="clearfix"></div>
-			</div>
 		</div>
+
+<?php } ?>
 	</div>
 
-<?php if ($count < $total) { ?>
-	<hr style="margin-top: 10px; margin-bottom: 10px;">
-<?php } ?>
-
-<?php } ?>
 <?php } else { ?>
 	<p><strong><?= __d('backend', 'You have no messages sent or received.'); ?></strong></p>
 	<br>
+
 <?php } ?>
 
 </div>
 
 <div class="row">
-	<hr>
-	<a class='btn btn-success col-sm-2' href='<?= site_url('admin/messages/create'); ?>'><i class='fa fa-send'></i> <?= __d('backend', 'Send a new Message'); ?></a>
 	<div class="pull-right">
 		<?= $messages->links(); ?>
 	</div>
