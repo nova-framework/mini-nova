6a3f2b6d9163d511c8853d573c175febbef8af98
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2017-05-26 12:10:51 +0300

    Overall improvements

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -34,6 +34,19 @@ return array(
 
 	'default' => 'primary',
 
+    /*
+    |--------------------------------------------------------------------------
+    | Migration Repository Table
+    |--------------------------------------------------------------------------
+    |
+    | This table keeps track of all the migrations that have already run for
+    | your application. Using this information, we can determine which of
+    | the migrations on disk haven't actually been run in the database.
+    |
+    */
+
+    'migrations' => 'migrations',
+
 	/*
 	|--------------------------------------------------------------------------
 	| Database Connections
