29bc7f7503f40aa0f3c59e1ceaa9d385ae85e5eb
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2017-05-26 21:04:55 +0300

    Add the default migrations

---
+++ app/Database/Migrations/2017_05_26_205445_create_cache_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Mini\Database\Schema\Blueprint;
+use Mini\Database\Migrations\Migration;
+
+
+class CreateCacheTable extends Migration 
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cache', function(Blueprint $table)
+        {
+            $table->string('key')->unique();
+            $table->text('value');
+            $table->integer('expiration');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('cache');
+    }
+
+}
