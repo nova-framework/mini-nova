e14ae1ec7be83a3672d79a7992321bf819be5acb
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2017-05-27 15:29:42 +0300

    Improve the FileManager plugin

--- plugins/FileManager/src/Bootstrap.php
+++ plugins/FileManager/src/Bootstrap.php
@@ -14,19 +14,19 @@
  */
 Event::listen('backend.menu', function($user)
 {
-	if ($user->hasRole('administrator')) {
-		$items = array(
-			array(
-				'uri'	=> 'admin/files',
-				'title'  => __d('files', 'Files'),
-				'label'  => '',
-				'icon'   => 'file',
-				'weight' => 3,
-			),
-		);
-	} else {
-		$items = array();
+	if (! $user->hasRole('administrator')) {
+		return array();
 	}
+
+	$items = array(
+		array(
+			'uri'		=> 'admin/files',
+			'title'		=> __d('files', 'Files'),
+			'label'		=> '',
+			'icon'		=> 'file',
+			'weight'	=> 3,
+		),
+	);
 
 	return $items;
 });
