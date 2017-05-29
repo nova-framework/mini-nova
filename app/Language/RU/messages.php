2ef4c84de4a867701743dc31d60138062a8b0e09
Victor Efremov <victor.efremov@gmail.com>
2017-05-29 22:27:23 +0300

    гзвфеу lang RU

--- app/Language/RU/messages.php
+++ app/Language/RU/messages.php
@@ -1,15 +1,15 @@
 <?php
 
 return array (
-  'Elapsed Time: <b>{0}</b> sec | Memory Usage: <b>{1}</b> | SQL: <b>{2}</b> {3, plural, one{query} other{queries}} | UMAX: <b>{4}</b>' => '',
-  'Elapsed Time: <b>{0}</b> sec | Memory Usage: <b>{1}</b> | UMAX: <b>{2}</b>' => '',
-  'Internal Server Error' => '',
-  'Method not allowed' => '',
-  'Service unavailable' => '',
-  'Something has gone wrong on the Web Server.' => '',
-  'This could be the result of an invalid Page request.' => '',
-  'This site is temporarily unavailable.' => '',
-  'Unauthorized' => '',
-  'Welcome' => '',
-  'Yep! It works.' => '',
+  'Elapsed Time: <b>{0}</b> sec | Используемая память: <b>{1}</b> | SQL: <b>{2}</b> {3, мн.ч., один{запрос} остальные{запросов}} | UMAX: <b>{4}</b>' => '',
+  'Elapsed Time: <b>{0}</b> sec | Используемая память: <b>{1}</b> | UMAX: <b>{2}</b>' => '',
+  'Internal Server Error' => 'Внутрення ошибка сервера',
+  'Method not allowed' => 'Метод не разрешен',
+  'Service unavailable' => 'Сервис недоступен',
+  'Something has gone wrong on the Web Server.' => 'Ошибка на сервере',
+  'This could be the result of an invalid Page request.' => 'Это может быть результатом неверного запроса страницы.',
+  'This site is temporarily unavailable.' => 'Сайт временно недоступен.',
+  'Unauthorized' => 'Неразрешенный',
+  'Welcome' => 'Добро пожаловать',
+  'Yep! It works.' => 'Это работает!',
 );
