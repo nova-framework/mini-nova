ac1158c64849ac17272a8b2a93444c71b99197b1
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2017-05-29 16:43:07 +0300

    Update the Language files

--- plugins/Content/src/Language/RO/messages.php
+++ plugins/Content/src/Language/RO/messages.php
@@ -1,6 +1,8 @@
 <?php
 
 return array (
+  'Content' => '',
+  'Homepage' => '',
   'Welcome to the Content Plugin' => '',
   'Yep! It works.' => '',
 );
--- plugins/Content/src/Language/RU/messages.php
+++ plugins/Content/src/Language/RU/messages.php
@@ -1,6 +1,8 @@
 <?php
 
 return array (
+  'Content' => '',
+  'Homepage' => '',
   'Welcome to the Content Plugin' => 'Добро пожаловть в плагин <b>Контент</b>',
   'Yep! It works.' => 'ОК, это работает',
 );
