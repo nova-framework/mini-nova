f769e7f30a78586225e118fa9aa8fa5031be08bd
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2017-05-29 16:03:49 +0300

    Improve the Blocks plugin

--- plugins/Blocks/src/Support/BlockManager.php
+++ plugins/Blocks/src/Support/BlockManager.php
@@ -31,25 +31,70 @@ class BlockManager
 
 	public function render($area)
 	{
-		$authMode = Auth::check() ? 'auth' : 'guest';
+		$mode = Auth::check() ? 'auth' : 'guest';
 
-		$blocks = Block::where('area', $area)->where(function ($query) use ($authMode)
+		$blocks = Block::where('area', $area)->where(function ($query) use ($mode)
 		{
-			return $query->whereNull('auth_mode')->orWhere('auth_mode', $authMode);
+			return $query->whereNull('auth_mode')->orWhere('auth_mode', $mode);
 
 		})->get();
 
-		//
-		$result = '';
-
-		foreach ($blocks as $block) {
-			$result .= $this->gatherBlockContent($block);
-		}
-
-		return $result;
+		return $this->processBlocks($blocks, $mode);
 	}
 
-	protected function gatherBlockContent(Block $block)
+	protected function processBlocks($blocks, $mode)
+	{
+		$results = array();
+
+		foreach ($blocks as $block) {
+			if ($this->blockIsVisible($block, $mode)) {
+				$results[] = $this->getBlockContent($block);
+			}
+		}
+
+		return implode(PHP_EOL, $results);
+	}
+
+	protected function blockIsVisible($block, $mode)
+	{
+		return $this->userHasAccess($block, $mode);
+	}
+
+	protected function userHasAccess($block, $mode)
+	{
+		if (is_null($block->auth_mode)) {
+			return true;
+		}
+
+		// If we are on the mode 'guest'
+		else if ($mode === 'guest') {
+			return ($block->auth_mode === 'guest');
+		}
+
+		// We are on the mode 'auth'
+		else if ($mode === 'auth') {
+			$user = Auth::user();
+
+			if ($block->auth_mode === 'auth') {
+				if (isset($block->user_roles)) {
+					$roles = array_filter(explode(',', $block->user_roles), function($value)
+					{
+						return ! empty($value);
+					});
+
+					if (! empty($roles)) {
+						return $user->hasRole($roles);
+					}
+				}
+
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	protected function getBlockContent(Block $block)
 	{
 		$content = '';
 
