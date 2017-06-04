7e6d02f4eca091de6ac4673fe3a0950ba1bae8aa
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2017-06-04 10:49:11 +0300

    Improve the Backend plugin

--- plugins/Backend/src/Views/Layouts/Default.php
+++ plugins/Backend/src/Views/Layouts/Default.php
@@ -21,103 +21,101 @@
 
 <div id="wrapper">
 	<div class="container-fluid">
+		<!-- Navigation -->
+		<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand sidebar-toggle" href="#menu-toggle" id="menu-toggle">
+					<i class='fa fa-bars'></i>
+				</a>
+				<a class="navbar-brand" style="margin-top: 1px;" href="<?= site_url('admin/dashboard'); ?>"> <strong><?= __d('backend', 'Control Panel'); ?></strong></a>
+			</div>
 
-	<!-- Navigation -->
-	<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
-		<div class="navbar-header">
-			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
-				<span class="sr-only">Toggle navigation</span>
-				<span class="icon-bar"></span>
-				<span class="icon-bar"></span>
-				<span class="icon-bar"></span>
-			</button>
-			<a class="navbar-brand sidebar-toggle" href="#menu-toggle" id="menu-toggle">
-				<i class='fa fa-bars'></i>
-			</a>
-			<a class="navbar-brand" style="margin-top: 1px;" href="<?= site_url('admin/dashboard'); ?>"> <strong><?= __d('backend', 'Control Panel'); ?></strong></a>
-		</div>
+			<!-- Collect the nav links, forms, and other content for toggling -->
+			<div class="collapse navbar-collapse navbar-ex1-collapse">
+				<ul class="nav navbar-nav navbar-right">
+					<li <?php if($baseUri == 'admin/messages') echo 'class="active"'; ?>>
+						<a href="<?= site_url('admin/messages'); ?>" title="<?= __d('backend', 'Your Messages'); ?>">
+							<i class='fa fa-envelope'></i>
+							<?= __d('backend', 'Messages'); ?> <?php if (isset($privateMessageCount) && ($privateMessageCount > 0)) echo '<span class="label label-success">' .$privateMessageCount .'</span>'; ?>
+						</a>
+					</li>
+					<li <?php if($baseUri == 'admin/notifications') echo 'class="active"'; ?>>
+						<a href="<?= site_url('admin/notifications'); ?>" title="<?= __d('backend', 'Your Notifications'); ?>">
+							<i class='fa fa-bell'></i>
+							<?= __d('backend', 'Notifications'); ?> <?php if (isset($notificationCount) && ($notificationCount > 0)) echo '<span class="label label-success">' .$notificationCount .'</span>'; ?>
+						</a>
+					</li>
+					<li class="dropdown">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false" title="<?= Language::name() .' (' .Language::code() .')'; ?>">
+							<i class='fa fa-language'></i> <?= Language::name(); ?> <span class="caret"></span>
+						</a>
+						<ul class="dropdown-menu">
+							<?php foreach (Config::get('languages') as $code => $info) { ?>
+							<li <?= ($code == Language::code()) ? 'class="active"' : ''; ?>>
+								<a href='<?= site_url('language/' .$code) ?>' title='<?= $info['info'] ?>'><i class='fa fa-circle-o'></i> <?= $info['name'] .' (' .$code .')' ?></a>
+							</li>
+							<?php } ?>
+						</ul>
+					</li>
+					<!-- Authentication Links -->
+					<li class="dropdown <?= ($baseUri == 'admin/profile') ? 'active' : ''; ?>">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" title="<?= $currentUser->fullName() ?>">
+							<img src="<?= $currentUser->picture(); ?>" class="user-image" alt="User Image"> <?= $currentUser->username ?> <span class="caret"></span>
+						</a>
+						<ul class="dropdown-menu" role="menu">
+							<li <?= ($baseUri == 'admin/profile') ? 'class="active"' : ''; ?>>
+								<a href="<?= site_url('admin/profile'); ?>"><i class='fa fa-circle-o'></i> <?= __d('backend', 'Profile'); ?></a>
+							</li>
+							<li role="separator" class="divider"></li>
+							<li>
+								<a href="<?= site_url('auth/logout'); ?>"
+									onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+									<i class='fa fa-sign-out'></i> <?= __d('backend', 'Logout'); ?>
+								</a>
+								<form id="logout-form" action="<?= site_url('auth/logout'); ?>" method="POST" style="display: none;"></form>
+							</li>
+						</ul>
+					</li>
+				</ul>
 
-		<!-- Collect the nav links, forms, and other content for toggling -->
-		<div class="collapse navbar-collapse navbar-ex1-collapse">
-			<ul class="nav navbar-nav navbar-right">
-				<li <?php if($baseUri == 'admin/messages') echo 'class="active"'; ?>>
-					<a href="<?= site_url('admin/messages'); ?>" title="<?= __d('backend', 'Your Messages'); ?>">
-						<i class='fa fa-envelope'></i>
-						<?= __d('backend', 'Messages'); ?> <?php if (isset($privateMessageCount) && ($privateMessageCount > 0)) echo '<span class="label label-success">' .$privateMessageCount .'</span>'; ?>
-					</a>
-				</li>
-				<li <?php if($baseUri == 'admin/notifications') echo 'class="active"'; ?>>
-					<a href="<?= site_url('admin/notifications'); ?>" title="<?= __d('backend', 'Your Notifications'); ?>">
-						<i class='fa fa-bell'></i>
-						<?= __d('backend', 'Notifications'); ?> <?php if (isset($notificationCount) && ($notificationCount > 0)) echo '<span class="label label-success">' .$notificationCount .'</span>'; ?>
-					</a>
-				</li>
-				<li class="dropdown">
-					<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false" title="<?= Language::name() .' (' .Language::code() .')'; ?>">
-						<i class='fa fa-language'></i> <?= Language::name(); ?> <span class="caret"></span>
-					</a>
-					<ul class="dropdown-menu">
-					<?php foreach (Config::get('languages') as $code => $info) { ?>
-						<li <?= ($code == Language::code()) ? 'class="active"' : ''; ?>>
-							<a href='<?= site_url('language/' .$code) ?>' title='<?= $info['info'] ?>'><i class='fa fa-circle-o'></i> <?= $info['name'] .' (' .$code .')' ?></a>
-						</li>
+				<!-- Sidebar Menu Items - These collapse to the responsive navigation menu on small screens -->
+				<ul class="nav navbar-nav side-nav" id="side-menu">
+					<li class="header">ADMINISTRATION</li>
+					<?php foreach ($menuItems as $count => $item) { ?>
+					<?php $children = Arr::get($item, 'children', array()); ?>
+					<?php if (! empty($children)) { ?>
+					<?php $active = in_array($currentUri, Arr::pluck($children, 'uri')); ?>
+					<li <?= $active ? 'class="active"' : ''; ?>>
+						<a href="#" class="collapse-toggle <?= ! $active ? 'collapsed' : ''; ?>" data-toggle="collapse" data-target="#menu-children-<?= $count; ?>" aria-expanded="false" role="button">
+							<i class="fa fa-<?= $item['icon'] ?>"></i> <?= $item['title']; ?>
+							<span class="pull-right-container">
+								<i class="fa fa-angle-left pull-right"></i>
+							</span>
+						</a>
+						<ul id="menu-children-<?= $count; ?>" class="nav nav-second-level collapse <?= $active ? 'in' : ''; ?>">
+						<?php foreach ($children as $child) { ?>
+							<li <?= ($currentUri == $child['uri']) ? 'class="active"' : ''; ?>>
+								<a href="<?= site_url($child['uri']); ?>"><i class="fa fa-circle-o"></i> <?= $child['title']; ?> <?= $child['label']; ?></a>
+							</li>
+						<?php } ?>
+						</ul>
+					</li>
+					<?php } else { ?>
+					<li <?= ($baseUri == $item['uri']) ? 'class="active"' : ''; ?>>
+						<a href="<?= site_url($item['uri']); ?>"><i class="fa fa-<?= $item['icon'] ?>"></i> <?= $item['title']; ?> <?= $item['label']; ?></a>
+					</li>
 					<?php } ?>
-					</ul>
-				</li>
-				<!-- Authentication Links -->
-				<li class="dropdown <?= ($baseUri == 'admin/profile') ? 'active' : ''; ?>">
-					<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" title="<?= $currentUser->fullName() ?>">
-						<img src="<?= $currentUser->picture(); ?>" class="user-image" alt="User Image"> <?= $currentUser->username ?> <span class="caret"></span>
-					</a>
-					<ul class="dropdown-menu" role="menu">
-						<li <?= ($baseUri == 'admin/profile') ? 'class="active"' : ''; ?>>
-							<a href="<?= site_url('admin/profile'); ?>"><i class='fa fa-circle-o'></i> <?= __d('backend', 'Profile'); ?></a>
-						</li>
-						<li role="separator" class="divider"></li>
-						<li>
-							<a href="<?= site_url('auth/logout'); ?>"
-								onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-								<i class='fa fa-sign-out'></i> <?= __d('backend', 'Logout'); ?>
-							</a>
-							<form id="logout-form" action="<?= site_url('auth/logout'); ?>" method="POST" style="display: none;"></form>
-						</li>
-					</ul>
-				</li>
-			</ul>
-
-			<!-- Sidebar Menu Items - These collapse to the responsive navigation menu on small screens -->
-			<ul class="nav navbar-nav side-nav" id="side-menu">
-				<li class="header">ADMINISTRATION</li>
-			<?php foreach ($menuItems as $count => $item) { ?>
-				<?php $children = Arr::get($item, 'children', array()); ?>
-				<?php if (! empty($children)) { ?>
-				<?php $active = in_array($currentUri, Arr::pluck($children, 'uri')); ?>
-				<li <?= $active ? 'class="active"' : ''; ?>>
-					<a href="#" class="collapse-toggle <?= ! $active ? 'collapsed' : ''; ?>" data-toggle="collapse" data-target="#menu-children-<?= $count; ?>" aria-expanded="false" role="button">
-						<i class="fa fa-<?= $item['icon'] ?>"></i> <?= $item['title']; ?>
-						<span class="pull-right-container">
-							<i class="fa fa-angle-left pull-right"></i>
-						</span>
-					</a>
-					<ul id="menu-children-<?= $count; ?>" class="nav nav-second-level collapse <?= $active ? 'in' : ''; ?>">
-					<?php foreach ($children as $child) { ?>
-						<li <?= ($currentUri == $child['uri']) ? 'class="active"' : ''; ?>>
-							<a href="<?= site_url($child['uri']); ?>"><i class="fa fa-circle-o"></i> <?= $child['title']; ?> <?= $child['label']; ?></a>
-						</li>
 					<?php } ?>
-					</ul>
-				</li>
-				<?php } else { ?>
-				<li <?= ($baseUri == $item['uri']) ? 'class="active"' : ''; ?>>
-					<a href="<?= site_url($item['uri']); ?>"><i class="fa fa-<?= $item['icon'] ?>"></i> <?= $item['title']; ?> <?= $item['label']; ?></a>
-				</li>
-				<?php } ?>
-			<?php } ?>
-			</ul>
-		</div>
-	<!-- /.container -->
-	</nav>
-
+				</ul>
+			</div>
+		<!-- /.container -->
+		</nav>
 	</div>
 
 	<div id="page-wrapper">
@@ -125,20 +123,20 @@
 			<?= $content; ?>
 		<div>
 
-			<footer id="footer" class="footer">
-				<div class="container-fluid">
-					<div class="row" style="margin: 15px 0 0;">
-						<div class="col-lg-4">
-							Mini MVC Framework <strong><?= VERSION; ?></strong> / Kernel <strong><?= App::version(); ?></strong>
-						</div>
-						<div class="col-lg-8">
-							<p class="text-muted pull-right">
-								<small><!-- DO NOT DELETE! - Profiler --></small>
-							</p>
-						</div>
+		<footer id="footer" class="footer">
+			<div class="container-fluid">
+				<div class="row" style="margin: 15px 0 0;">
+					<div class="col-lg-4">
+						Mini MVC Framework <strong><?= VERSION; ?></strong> / Kernel <strong><?= App::version(); ?></strong>
+					</div>
+					<div class="col-lg-8">
+						<p class="text-muted pull-right">
+							<small><!-- DO NOT DELETE! - Profiler --></small>
+						</p>
 					</div>
 				</div>
-			</footer>
+			</div>
+		</footer>
 	</div>
 </div>
 
