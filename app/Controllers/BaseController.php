38525460274cfa6975cd8b20b84117dced51eef6
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2017-06-10 06:38:44 +0300

    Improve the App\Controllers\BaseController

--- app/Controllers/BaseController.php
+++ app/Controllers/BaseController.php
@@ -66,13 +66,6 @@ class BaseController extends Controller
 	 */
 	protected $viewVars = array();
 
-	/**
-	 * The (alternative) Response instance.
-	 *
-	 * @var \Mini\Http\Response|null
-	 */
-	protected $response;
-
 
 	/**
 	 * Create a new Controller instance.
@@ -101,7 +94,7 @@ class BaseController extends Controller
 
 		$response = call_user_func_array(array($this, $method), $parameters);
 
-		return $this->after($response);
+		return $this->processResponse($response);
 	}
 
 	/**
@@ -121,10 +114,8 @@ class BaseController extends Controller
 	 *
 	 * @return mixed
 	 */
-	protected function after($response)
+	protected function processResponse($response)
 	{
-		$response = $response ?: $this->response;
-
 		if (! $this->autoRender()) {
 			return $this->prepareResponse($response);
 		} else if (is_null($response)) {
@@ -132,76 +123,21 @@ class BaseController extends Controller
 		}
 
 		if (($response instanceof RenderableInterface) && $this->autoLayout()) {
-			return $this->renderWhithinLayout($response);
+			// Convert the used theme to a View namespace.
+			$namespace = ! empty($this->theme) ? $this->theme .'::' : '';
 
+			// Compute the full name of View used as layout.
+			$view = sprintf('%sLayouts/%s', $namespace, $this->layout);
+
+			// Create a View instance.
+			$layout = View::make($view, array_merge($this->viewVars, array(
+				'content' => $response
+			)));
+
+			return new Response($layout);
 		}
 
 		return $this->prepareResponse($response);
-	}
-
-	/**
-	 * Internally redirects one action to another.
-	 *
-	 * @param string $action The new action to be 'redirected' to.
-	 * @return mixed Returns the return value of the called action
-	*/
-	public function setAction($action)
-	{
-		$this->action = $action;
-
-		return call_user_func_array(
-			array($this, $action), array_slice(func_get_args(), 1)
-		);
-	}
-
-	/**
-	 * Instantiates the correct View, hands it its data, and uses it to render the view output.
-	 *
-	 * @param string $view View to use for rendering
-	 * @param string $layout Layout to use
-	 * @return \Mini\Http\Response A response object containing the rendered view.
-	 */
-	public function render($view = null, $layout = null)
-	{
-		$this->autoRender = false;
-
-		// Create the quelified View instance.
-		$view = $this->createView(array(), $view);
-
-		if ($this->autoLayout()) {
-			$response = $this->renderWhithinLayout($view, $layout);
-		} else {
-			$response = new Response($view);
-		}
-
-		return $this->response = $response;
-	}
-
-	/**
-	 * Handle a RenderableInterface implementation.
-	 *
-	 * @param \Mini\Support\Contracts\RenderableInterface  $view
-	 * @return \Mini\Http\Response
-	 */
-	protected function renderWhithinLayout(RenderableInterface $renderable, $layout = null)
-	{
-		$layout = $layout ?: $this->layout;
-
-		// Convert the used theme to a View namespace.
-		$namespace = ! empty($this->theme) ? $this->theme .'::' : '';
-
-		// Compute the name of View used as layout.
-		$view = sprintf('%sLayouts/%s', $namespace, $layout);
-
-		// Compute the composite View data.
-		$data = array_merge($this->viewVars, array(
-			'content' => $renderable
-		));
-
-		// Create and render the layout's View.
-		$content = View::make($view, $data)->render();
-
-		return new Response($content);
 	}
 
 	/**
@@ -269,11 +205,7 @@ class BaseController extends Controller
 	protected function set($one, $two = null)
 	{
 		if (is_array($one)) {
-			if (is_array($two)) {
-				$data = array_combine($one, $two);
-			} else {
-				$data = $one;
-			}
+			$data = is_array($two) ? array_combine($one, $two) : $one;
 		} else {
 			$data = array($one => $two);
 		}
@@ -333,16 +265,6 @@ class BaseController extends Controller
 	}
 
 	/**
-	 * Return the current called action.
-	 *
-	 * @return string
-	 */
-	public function getAction()
-	{
-		return $this->action;
-	}
-
-	/**
 	 * Return the current Theme.
 	 *
 	 * @return string
@@ -360,15 +282,5 @@ class BaseController extends Controller
 	public function getLayout()
 	{
 		return $this->layout;
-	}
-
-	/**
-	 * Return the current View data.
-	 *
-	 * @return array
-	 */
-	public function getData()
-	{
-		return $this->viewVars;
 	}
 }
