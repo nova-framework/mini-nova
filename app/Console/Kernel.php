b33b73dbe96024537d6336f9a1e1350643f9694e
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2017-06-17 09:51:57 +0300

    Improve the App\Console\Kernel

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use Mini\Console\Scheduling\Schedule;
 use Mini\Foundation\Console\Kernel as ConsoleKernel;
 
 
@@ -14,4 +15,16 @@ class Kernel extends ConsoleKernel
 	 */
 	protected $commands = array();
 
+
+	/**
+	 * Define the application's command schedule.
+	 *
+	 * @param  \Mini\Console\Scheduling\Schedule  $schedule
+	 * @return void
+	 */
+	protected function schedule(Schedule $schedule)
+	{
+		$schedule->command('mailer:queue:flush')
+			->everyMinute();
+	}
 }
